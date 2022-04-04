012a36383df8c6b4dac19796591b39583633acd5
dandeville <dandeville26@gmail.com>
2022-04-03 23:44:44 -0500

    Se avanza en los estilos y se ajustan las acciones del navbar

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -11,3 +11,45 @@ navbarClose.addEventListener('click', () => {
   navbarMenu.classList.remove('navbar-menu_visible')
 })
 
+
+const menuLinks = document.querySelectorAll('.navbar-menu a[href^="#"]')
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', function() {
+    navbarMenu.classList.remove('navbar-menu_visible')
+  })
+})
+
+const form = document.getElementById('form')
+const email = document.getElementById('email')
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	checkInputs()
+});
+
+function checkInputs() {
+
+	const emailValue = email.value.trim()
+
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank')
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email')
+	} else {
+		console.log('email is valid')
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small')
+	formControl.className = 'form-control error'
+	small.innerText = message;
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
